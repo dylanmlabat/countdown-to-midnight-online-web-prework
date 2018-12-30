f22889ca39d0b95df2690359509403425631b700
Dylan Michael Labat <dml252@live.com>
2018-12-30 18:44:17 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  while x > 0
+  puts "#{x} SECOND(S)!"
+  x -= 1
 end
+  
+  if x == 0
+   "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(x)
+  while x > 0
+  sleep 1
+  x -= 1
+end
+end    
